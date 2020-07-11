82835adbe1e24e5df519743a67273d4eb225db79
abechoi <abraham.choi@icloud.com>
2020-07-10 19:12:09 -0700

    Reading List App Part 2

---
+++ src/contexts/BookContext.js
@@ -0,0 +1,28 @@
+import React, { createContext, useState } from 'react';
+import { v4 } from 'uuid';
+
+export const BookContext = createContext();
+
+const BookContextProvider = (props) => {
+
+  const [ books, setBooks ] = useState([
+    { title: 'Name of the Wind', author: 'Patrick Rothfuss', id: 1 },
+    { title: 'The Final Empire', author: 'Brandon Sanderson', id: 2 },
+  ]);
+
+  const addBook = (title, author) => {
+    setBooks([...books, {title, author, id: v4()} ]);
+  };
+
+  const removeBook = (id) => {
+    setBooks(books.filter(book => book.id !== id));
+  };
+
+  return (
+    <BookContext.Provider value={{ books, addBook, removeBook }}>
+      {props.children}
+    </BookContext.Provider>
+  );
+}
+
+export default BookContextProvider;
